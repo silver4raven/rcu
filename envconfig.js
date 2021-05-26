b82a92af8cfdee086e2e837fff89b90594a12ab9
silver4raven <silver.raven@hotmail.com>
2021-05-26 12:27:59 +0300

    Add files via upload

---
+++ envconfig.js
@@ -0,0 +1,9 @@
+window.customOptions = {
+    customTVType: 'LG',
+    customIP: '127.0.0.1', //'10.10.1.101',
+    customPort: '9999', //'1111',
+    customRoomNo: '201', // 'LGProLW641',
+    customTVID: '1234',  // '4351',
+}
+
+window.themeColors = {primary: '#f26522', background: '#000000', font: '#888888', fontB:'#000000', }
